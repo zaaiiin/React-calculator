604a2e3dd84fbd596dfce42d803a3331eea3936e
zaaiiin <zaainnaseem@gmail.com>
2022-08-17 15:17:49 +0800

    Calculator interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+      <div className="display">
+      0
+      </div>
+
+      <div className="allButtons">
+
+      
+      <button className="deleteButton">DEL</button>
+      
+
+      <button className="operators">÷</button>
+      <button className="digits">7</button>
+      <button className="digits">8</button>
+      <button className="digits">9</button>
+
+      <button className="operators">×</button>
+      <button className="digits">4</button>
+      <button className="digits">5</button>
+      <button className="digits">6</button>
+
+      <button className="operators">-</button>
+      <button className="digits">1</button>
+      <button className="digits">2</button>
+      <button className="digits">3</button>
+
+      <button className="operators">+</button>
+      
+      <button className="zero">0</button>
+
+      <button className="dotSign">.</button>
+      
+      <button className="equalSign">=</button>
+
+
+      </div>
+
+    </div>
     </div>
   );
 }
